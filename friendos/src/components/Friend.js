b7278fea4373f8ce2176963d01095170621928d9
Schrese <53787633+Schrese@users.noreply.github.com>
2019-12-17 19:31:44 -0500

    trying to style just a bit before moving on

--- friendos/src/components/Friend.js
+++ friendos/src/components/Friend.js
@@ -1,13 +1,23 @@
 import React from 'react';
-//For editing the friends
+import styled from 'styled-components';
+
 const Friend = ({friend}) => {
     return (
-        <div>
+        <FriendContainer>
             <h1>{friend.name}</h1>
             <p>Age: {friend.age}</p>
             <p>email: {friend.email}</p>
-        </div>
+        </FriendContainer>
     )
 }
 
 export default Friend
+
+const FriendContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 300px;
+    border: 2px solid grey;
+    background: lightblue;
+    margin: 0 auto;
+`
